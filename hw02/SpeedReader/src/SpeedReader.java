c46d1a1bff47d5f2b4306b1786734fb62be46d7d
Joey Lederman <lederman17@grinnell.edu>
2016-02-04 23:02:31 -0600

    Adding Beta code

--- hw02/SpeedReader/src/SpeedReader.java
+++ hw02/SpeedReader/src/SpeedReader.java
@@ -12,20 +12,24 @@ public class SpeedReader {
 			int MsecPerWord = 60000 / wpm;
 			int width = Integer.parseInt(args[1]);
 			int height = Integer.parseInt(args[2]);
+			int stringWidth;
+			int stringHeight;
+			String word;
 			WordGenerator wordGen = new WordGenerator(args[0]);
 		    DrawingPanel panel = new DrawingPanel(width, height);
 		    Graphics g = panel.getGraphics();
 		    Font f = new Font("Courier", Font.BOLD, Integer.parseInt(args[3]));
 		    g.setFont(f);
-		    g.setColor(Color.blue);
 		    while(wordGen.hasNext()){
-			    g.clearRect(0,0,width,height);
-			    g.drawString(wordGen.next(), 50, 50);
+		    	word = wordGen.next();
+			    stringWidth = (width / 2) - (g.getFontMetrics().stringWidth(word) / 2);
+			    stringHeight = (height / 2) + 10;
+			    g.drawString(word, stringWidth, stringHeight);
 			    Thread.sleep(MsecPerWord);
+		    	panel.clear();
 		    }
 			System.out.println("Word count: " + wordGen.getWordCount());
-			System.out.println("Word count: " + wordGen.getSentenceCount());
+			System.out.println("Sentence count: " + wordGen.getSentenceCount());
 		}
 	}
-
 }
