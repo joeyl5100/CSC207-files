61ecef4e0e724208fc3a409d868b61427516e914
Joey Lederman <lederman17@grinnell.edu>
2016-02-05 15:34:19 -0600

    Adding javadocs to code

--- hw02/SpeedReader/src/SpeedReader.java
+++ hw02/SpeedReader/src/SpeedReader.java
@@ -1,19 +1,27 @@
 import java.awt.*;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class SpeedReader {
+	public static final int MILLISEC_PER_MINUTE = 60000;
 
+	/**
+	 * Instantiates a word generator and drawing panel to display words from file 
+	 * at a specified speed in a customized panel and font size
+	 * 
+	 * @param args takes filename, width, height, font size, and wpm in that order
+	 * @throws IOException if file from command-line cannot be found
+	 * @throws InterruptedException
+	 */
 	public static void main(String[] args) throws IOException, InterruptedException{
 		if(args.length != 5){
 			System.out.println("Error, incorrect arguments given");
 		}
 		else{
 			int wpm = Integer.parseInt(args[4]);
-			int MsecPerWord = 60000 / wpm;
+			int msecPerWord =  MILLISEC_PER_MINUTE / wpm;
 			int width = Integer.parseInt(args[1]);
 			int height = Integer.parseInt(args[2]);
-			int stringWidth;
-			int stringHeight;
 			String word;
 			WordGenerator wordGen = new WordGenerator(args[0]);
 		    DrawingPanel panel = new DrawingPanel(width, height);
@@ -22,10 +30,10 @@ public class SpeedReader {
 		    g.setFont(f);
 		    while(wordGen.hasNext()){
 		    	word = wordGen.next();
-			    stringWidth = (width / 2) - (g.getFontMetrics().stringWidth(word) / 2);
-			    stringHeight = (height / 2) + 10;
+			    int stringWidth = (width / 2) - (g.getFontMetrics().stringWidth(word) / 2); //Center text to window
+			    int stringHeight = (height / 2) + (f.getSize() / 4);			    
 			    g.drawString(word, stringWidth, stringHeight);
-			    Thread.sleep(MsecPerWord);
+			    Thread.sleep(msecPerWord);
 		    	panel.clear();
 		    }
 			System.out.println("Word count: " + wordGen.getWordCount());
--- hw02/SpeedReader/src/WordGenerator.java
+++ hw02/SpeedReader/src/WordGenerator.java
@@ -3,19 +3,35 @@ import java.io.File;
 import java.io.IOException;
 
 public class WordGenerator {
-	public int wordCount;
-	public int sentenceCount;
+	private int wordCount;
+	private int sentenceCount;
 	Scanner text;
+	
+	/**
+	 * Constructor of WordGenerator, initializing values and Scanner
+	 * to read file parameter
+	 * 
+	 * @param filename (with extensions) of the new input of Scanner 
+	 * @throws IOException if Scanner cannot connect to file
+	 */
 	public WordGenerator(String filename) throws IOException{
 		text = new Scanner(new File(filename));
 		this.wordCount = 0;
 		this.sentenceCount = 0;
 	}
-	
+	/**
+	 * Check to see if Scanner has another token to read
+	 * 
+	 * @return true if there is another token, otherwise returns false
+	 */
 	public boolean hasNext(){
 		return text.hasNext();
 	}
-	
+	/**
+	 * Returns next token and updates word count and sentence count
+	 * 
+	 * @return the next String token from Scanner
+	 */
 	public String next(){
 		this.wordCount ++;
 		String str = text.next();
@@ -25,10 +41,20 @@ public class WordGenerator {
 		return str;
 	}
 	
+	/**
+	 * Returns word count
+	 * 
+	 * @return number of words read
+	 */
 	public int getWordCount(){
 		return wordCount;
 	}
 
+	/**
+	 * Returns sentence count
+	 * 
+	 * @return number of sentences (that end in '.', '?', or '!')
+	 */
 	public int getSentenceCount(){
 		return sentenceCount;
 	}
