887764957df88da1dbfe8d2e442f284ea06ee30f
Joey Lederman <lederman17@grinnell.edu>
2016-02-04 19:53:36 -0600

    Adding incomplete WordGenerator

---
+++ hw02/SpeedReader/src/WordGenerator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class WordGenerator {
+	public int wordCount;
+	public int sentenceCount;
+	Scanner text;
+	public WordGenerator(String filename) throws IOException{
+		text = new Scanner(new File(filename));
+		this.wordCount = 0;
+		this.sentenceCount = 0;
+	}
+	
+	public boolean hasNext(){
+		return text.hasNext();
+	}
+	
+	public String next(){
+		this.wordCount ++;
+		String str = text.next();
+		if (str.equals(".") || str.equals("!") || str.equals("?")){
+			sentenceCount++;
+		}
+		return str;
+	}
+	
+	public int getWordCount(){
+		return wordCount;
+	}
+
+	public int getSentenceCount(){
+		return sentenceCount;
+	}
+}
